afcef203c70237da77c64bbf2e522d63c4aef09e
Kevin Valdez <kevinalexisvaldezz@hotmail.com>
2021-10-16 01:07:51 -0600

    Co-authored-by: ximenalopezm <ximenalopezm@users.noreply.github.com>

---
+++ proyectoBattleship.py
@@ -0,0 +1,94 @@
+# Ximena Lopez y Kevin Valdez
+
+# 14 de Oct del 2021
+
+# Proyecto Battleship
+
+import random
+import time
+
+
+""" Requisitos: 
+* Crear un tablero simulado en un .txt por renglones y columnas                                                 
+* Ambos jugadores pueden bajar el archivo de Github                                                                                                                      
+* Cada que un jugador ataca, se guarda el daño y se regreasa el archivo a Github                                                                                                          
+* No se debe permitir a un jugador jugar cuando es el turno del otro y prevenir si esto ocurre                                                                                                                
+* El tamaño del tablero puede ser fijo o mediante INPUT al igual que las naves
+* La ubicacion de las naves debe ser random y que no se encimnen 
+* Se recomienda que exista un procedimiento de inicialización de dimensiones , número de naves por jugador y localización                                                
+* El tablero debe ser un numero igual para ambos jugadores
+ """
+
+# Crear el tablero en el txt battleship, abrirlo y crearlo como matriz
+# Pedir cantidad de barcos a generar
+# Definir tamaño del tablero
+# Colocar barcos random
+# transformar tablero a txt 
+
+def comprobarControl():
+
+    fo = open("control.txt", "r")
+    turno = fo.readline()
+    fo.close()
+
+    turno = turno.split(" ")
+    if turno[0] == "1":
+        print("Es tu turno, hora de atacar!!")
+    else:
+        print("No es tu turno, no seas impaciente")
+
+def crearTablero(tablero):
+
+    arctxt = []
+
+    fo = open("battleship.txt", "r+")
+
+    while True:
+        linea = fo.readline()
+        if (not linea):
+            break
+        listaLinea = linea.split(' ')
+        arctxt.append(listaLinea)
+
+    fo.truncate(0)
+    fo.close()
+
+    for i in range(len(arctxt)):
+        for j in range(len(arctxt[0])):
+            tablero[i][j] = int(arctxt[i][j])
+
+def tableroaTXT(tablero):
+
+    fo = open("battleship.txt", "a")
+
+    for i in range(len(tablero)):
+        dato = ""
+        for j in range(len(tablero[0])):
+            dato = dato +" "+ str(tablero[i][j])
+        
+        fo.write(dato)
+        fo.write("\n")
+
+    fo.close()
+
+def main(): 
+    tablero = [[0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0],
+               [0,0,0,0,0,0,0,0,0,0]]
+
+    comprobarControl()
+    crearTablero(tablero)
+
+    for i in range(len(tablero)):
+        print(tablero[i])
+
+    tableroaTXT(tablero)
+
+main()
